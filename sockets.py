5bb8594fe93031ea0c4ef3483931890847ac56b0
fortheglory <deeplearningdude@gmail.com>
2017-11-10 20:31:56 +0800

    hero come back

--- sockets.py
+++
@@ -1,2 +0,0 @@
-print "leave for completing"
-print "we are happy with our code"
---
+++ test.py
@@ -0,0 +1,17 @@
+import sys
+print("hello")
+print(sys.executable)
+
+print(sys.version)
+
+
+class god():
+    pass
+
+
+class godness():
+    pass
+
+
+for num in [1, 2, 3, 4]:
+    print(num)
